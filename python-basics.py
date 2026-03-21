b39435a1ee36e51f5f04ce52d8c5da8f0f961b13
Louai <louaiberdai@gmail.comgit config --global user.name Louai>
2026-03-20 23:24:11 -0400

    Add error handlind for invalid input

--- python-basics.py
+++ python-basics.py
@@ -1,6 +1,9 @@
-année_de_naissance = input("Entrez votre année de naissance:  ").strip()
-nom = input("Entrez votre nom:  ").strip()
-année_MIT = 2032
-age = 2026 - int(année_de_naissance)
-difference = année_MIT - int(année_de_naissance)
-print(f"{nom}, votre age est de {age}ans. Il vous rest {difference} ans pour MIT.")
+try:
+    année_de_naissance = input("Entrez votre année de naissance:  ").strip()
+    nom = input("Entrez votre nom:  ").strip()
+    année_MIT = 2032
+    age = 2026 - int(année_de_naissance)
+    difference = année_MIT - int(année_de_naissance)
+    print(f"{nom}, votre age est de {age} ans. Il vous reste {difference} ans pour MIT.")
+except ValueError:
+    print("Erreur: Veuillez entrer une année valide (nombre entier).")
